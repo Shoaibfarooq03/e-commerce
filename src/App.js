d8a376c4b95156452dc368b64b093804736762da
Shoaibfarooq003 <shoaib.f@strahlenstudios.com>
2024-04-17 18:00:19 +0500

    complete home page ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
+
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </div>
-  </Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -141,8 +141,158 @@ const Home = () => {
           </div>
         </div>
         <div className="productbtn">
-            <button>Show More</button>
+          <button>Show More</button>
         </div>
+      </div>
+      <div class="explore">
+        <div class="explore-left">
+          <h1>50+ Beautifull Rooms</h1>
+          <p>Our designer already made a lot of beautiful</p>
+          <p className="exp-text">prototipe of rooms that inspire you</p>
+          <button>Explore More</button>
+        </div>
+        <div class="explore-center">
+          <img src="/explore.png" alt="Center Image" />
+        </div>
+        <div class="explore-right">
+          <img src="/explore2.png" alt="Right Image" />
+          <svg
+            width="78"
+            height="78"
+            viewBox="0 0 78 78"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="eclipse"
+          >
+            <g filter="url(#filter0_d_117_484)">
+              <circle cx="39" cy="35" r="24" fill="white" />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_117_484"
+                x="0"
+                y="0"
+                width="78"
+                height="78"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+                />
+                <feMorphology
+                  radius="1"
+                  operator="dilate"
+                  in="SourceAlpha"
+                  result="effect1_dropShadow_117_484"
+                />
+                <feOffset dy="4" />
+                <feGaussianBlur stdDeviation="7" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.16 0"
+                />
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_117_484"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_117_484"
+                  result="shape"
+                />
+              </filter>
+            </defs>
+          </svg>
+          <svg
+            width="9"
+            height="16"
+            viewBox="0 0 9 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="arrow"
+          >
+            <path
+              d="M1.00006 1L8.00006 8L1.00006 15"
+              stroke="#B88E2F"
+              stroke-width="1.8"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+        <div className="explore-end">
+          <img src="/explore3.png" alt="" />
+        </div>
+      </div>
+
+      <div className="furnitureImages">
+        <div className="title1">
+          Share your setup with
+          <p>#FurniroFurniture</p>
+        </div>
+        <div className="imgcontain">
+          <img src="/img1.png" alt="" className="img1" />
+          <img src="/img2.png" alt="" className="img2" />
+          <img src="/img3.png" alt="" className="img3" />
+          <img src="/img4.png" alt="" className="img4" />
+          <img src="/img5.png" alt="" className="img5" />
+          <img src="/img6.png" alt="" className="img6" />
+          <img src="/img7.png" alt="" className="img7" />
+          <img src="/img8.png" alt="" className="img8" />
+          <img src="/img9.png" alt="" className="img9" />
+        </div>
+      </div>
+
+      <div className="footerinfo">
+        <div className="portion1">
+          <h2>Funiro</h2>
+          <p className="t1">400 University Drive Suite 200 Coral</p>
+          <p className="t2">Gables</p>
+          <p className="t3"> FL 33134 USA</p>
+        </div>
+        <div className="portion2">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="portion3">
+          <h3>Help</h3>
+          <ul>
+            <li>Payment Option</li>
+            <li>Return</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="portion-horizontal">
+          <h3>Newsletter</h3>
+          <div class="subscription">
+            <p className="email">Enter your email address</p>
+            <p className="subs">SUBSCRIBE</p>
+          </div>
+        </div>
+      </div>
+      <div class="line2"></div>
+      <div className="footerinfobottom">
+        <div className="bottominfo">2023 furino. All rights reverved</div>
       </div>
     </div>
   );
